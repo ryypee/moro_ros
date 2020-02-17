56817b0514ad7780ec456f6523781089e00a3ac8
anton <blues.lead@gmail.com>
2020-02-17 15:37:10 +0200

    model works

--- moro_localization/src/ekf_localization.py
+++ moro_localization/src/ekf_localization.py
@@ -4,39 +4,41 @@ from std_msgs.msg import String
 from nav_msgs.msg import Odometry
 from marker_msgs.msg import MarkerDetection
 from filtering_utils.ekf import EKF
+import pickle
+from tf.transformations import euler_from_quaternion
+import signal
 
 ekf = EKF(3, 2, 2)
 
+odometry_history = []
+state_history = []
+gt_history = []
 
 def odom_callback(msg):
     rospy.loginfo("odometry message")
     ekf.predict(msg)
-    ekf.print_initials()
+    odometry_history.append(ekf.state_vector)
+    #ekf.print_initials()
     #pass
 
 
 def marker_callback(msg):
-    print(ekf.state_vector)
-    # TODO check length of msg came and perform update in a loop
-    # rospy.loginfo("Marker message")
     info = msg.markers
-    if len(info) == 0: # POSSIBLY ekf.predict.../ ekf.propagate_state...
-        #print("No transmitters found! State vector is", ekf.state_vector)
+    if len(info) == 0:
         return
-    #print(len(info))
     for i in range(len(info)):
-        #print("iteration", i)
-        #print(ekf.state_vector.shape)
         ekf.update(info[i])
-        pass
-    #pass
 
 
 def ekf_loc():
     rospy.init_node('ekf_localization', anonymous=True)
     rospy.Subscriber("odom", Odometry, odom_callback)
     rospy.Subscriber("base_marker_detection", MarkerDetection, marker_callback)
+    #rospy.Subscriber("base_pose_ground_truth", Odometry, collect_data)
     rospy.spin()
+
+
+
 
 
 if __name__ == '__main__':
